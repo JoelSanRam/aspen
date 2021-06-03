b696a8bfaff24582853c61eabcdad852b20c49f2
alexisCode50 <55004811+alexisCode50@users.noreply.github.com>
2021-06-03 11:40:56 -0500

    agregamos popup, las imagenes no hacen match

---
+++ js/popup.js
@@ -0,0 +1,31 @@
+// color1
+$('#color1').magnificPopup({
+    items: [
+      {
+        src: 'colores/1.jpg'
+      },
+      {
+        src: 'colores/2.jpg'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});
+
+// color1
+$('#color2').magnificPopup({
+    items: [
+      {
+        src: 'colores/2.jpg'
+      },
+      {
+        src: 'colores/3.jpg'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is default type
+});
